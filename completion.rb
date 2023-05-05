3d66d2ac174d3bb5e0db608b70f3ee40f0cddb6f
Tomasz Biłas <tomasz.bilas@gmail.com>
2023-05-05 16:04:28 +0200

    Add class to make completion requests

---
+++ completion.rb
@@ -0,0 +1,41 @@
+require 'yaml'
+require './http_caller'
+
+# Class performs chat completion requests
+# Configuration is loaded from ./config.yml
+class Completion
+  def initialize(config: {})
+    @url = 'https://api.openai.com/v1/chat/completions'
+    @config = YAML.load_file('config.yml')['openai_api'].transform_keys(&:to_sym)
+    @config.merge(config)
+  end
+
+  def call(content)
+    response = HttpCaller.make_post_request(url, body(content), headers)
+
+    total_tokens = response.parsed_response.dig('usage', 'total_tokens')
+    puts "Used: #{total_tokens} tokens"
+
+    response.parsed_response['choices'].first.dig('message', 'content')
+  end
+
+  private
+
+  attr_reader :config, :url
+
+  def body(content)
+    {
+      model: config[:model], 
+      max_tokens: config[:max_tokens],
+      temperature: config[:temperature],
+      messages: [{ role: config[:role], content: }]
+    }.to_json
+  end
+
+  def headers
+    {
+      'Content-Type' => 'application/json',
+      'Authorization' => "Bearer #{config[:api_key]}"
+    }
+  end
+end
